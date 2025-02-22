0ad1a3992bd244378e2019b9ee186ab0784b6d36
Montserrat Sendín <10212095+msendin@users.noreply.github.com>
2025-02-22 14:02:44 +0100

    Update MainActivity.kt
    removeLocationUpdates updated

--- app/src/main/java/com/google/android/gms/location/sample/locationupdates/MainActivity.kt
+++ app/src/main/java/com/google/android/gms/location/sample/locationupdates/MainActivity.kt
@@ -432,14 +432,7 @@ class MainActivity : AppCompatActivity() {
         // It is a good practice to remove location requests when the activity is in a paused or
         // stopped state. Doing so helps battery performance and is especially
         // recommended in applications that request frequent location updates.
-        mLocationCallback.let {
-            mFusedLocationClient.removeLocationUpdates(it).addOnCompleteListener {
-                mRequestingLocationUpdates = false
-                setButtonsEnabledState()
-            }
-            mRequestingLocationUpdates = false
-            setButtonsEnabledState()
-        }
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback)
         mRequestingLocationUpdates = false
         setButtonsEnabledState()
     }
